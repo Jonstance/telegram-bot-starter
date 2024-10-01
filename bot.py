43933cb68c68dc7d37f59a91c4c146fd71ba8058
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-10-01 21:25:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,8 +41,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton("Start now!", web_app={
             "url": "https://bot.snowdex.org"  # Replace with your actual mini app URL
         })],
-        [InlineKeyboardButton("Join Telegram Channel", url="https://t.me/snowcoin_App")],  # Replace with your channel URL
-        [InlineKeyboardButton("Follow on X", url="https://x.com/Snowcoin_App?t=JQJpsFgQuZRIo1qMP3Tmig&s=09")]  # Replace with your Twitter URL
+        [InlineKeyboardButton("Join Telegram Channel", url="https://t.me/snowdexlab")],  # Replace with your channel URL
+        [InlineKeyboardButton("Follow on X", url="https://x.com/SnowDex_org?t=T3Ic3PV2OdOIqP6XivhMcg&s=09")]  # Replace with your Twitter URL
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
